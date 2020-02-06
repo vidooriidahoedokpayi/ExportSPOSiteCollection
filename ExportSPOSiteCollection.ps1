b4769ebcc92ca3c6b2e73052c67896541025457f
Idaho Edokpayi <idaho.edokpayi@vidoori.com>
2020-02-06 10:29:02 -0500

    updates

--- ExportSPOSiteCollection.ps1
+++ ExportSPOSiteCollection.ps1
@@ -1,4 +1,25 @@
-﻿Function Combine-URI()
+﻿Function Create-Folder(){
+    param([Parameter(Mandatory=$true)][string]$folder)
+     if(-Not (Test-Path $folder)){
+        New-Item -Path $folder -ItemType directory;
+    }
+}
+Function Generate-File-Folder(){
+    param([Parameter(Mandatory=$true)][string]$fileName)
+     $folderName =  $fileName -replace '\s','_';
+     $folderName = $folderName -replace  '\.', '-';
+     $folderName = $folderName -replace  ',', '';
+     return $folderName;
+}
+
+Function Generate-Site-Folder(){
+    param([Parameter(Mandatory=$true)][string]$url)
+     $folderName =  $url -replace 'https://','';
+     $folderName = $folderName -replace  '.com', '';
+     return Generate-File-Folder -fileName $folderName;
+}
+
+Function Combine-URI()
 {
 param
     (
@@ -15,10 +36,15 @@ Function Process-Web()
 
     param
     (
-        [Parameter(Mandatory=$true)][Microsoft.SharePoint.Client.Web]$web
+        [Parameter(Mandatory=$true)][Microsoft.SharePoint.Client.Web]$web,
+        [Parameter(Mandatory=$true)][string]$rootFolder
+
     )
     $Ctx.Load($web.Lists);
     $Ctx.ExecuteQuery();
+    $webFolder = Generate-Site-Folder -url $web.Url;
+    $path = [System.IO.Path]::Combine($rootFolder, $webFolder);
+    Create-Folder -folder $path;
     foreach($list in $web.Lists){
         $Ctx.Load($list);
         $Ctx.ExecuteQuery();
@@ -27,16 +53,24 @@ Function Process-Web()
         Write-Host "Title: $ListTitle, Template: $BaseTemplate"
         if(($list.BaseTemplate -eq 101 ) -and ($list.Title -ne "Site Assets"))
         {
-            Get-DocumentLibraryFiles -Library $list
+            Get-DocumentLibraryFiles -Library $list -RootFolder $path
         }
     }
+
+    $Ctx.Load($web.Webs);
+    $Ctx.ExecuteQuery();
+    foreach($childWeb in $web.Webs){
+    Process-Web -web $childWeb -rootFolder $path
+}
+    return $path;
 }
 
 #Function to get all files of a folder
 Function Get-AllFilesFromFolder()
 {
     param(
-        [Parameter(Mandatory=$true)][Microsoft.SharePoint.Client.Folder]$Folder
+        [Parameter(Mandatory=$true)][Microsoft.SharePoint.Client.Folder]$Folder,
+        [Parameter(Mandatory=$true)][string]$RootFolder
     )
     #Get All Files of the Folder
     $Ctx =  $Folder.Context;
@@ -48,17 +82,14 @@ Function Get-AllFilesFromFolder()
     {
         #Get the File Name or do something
         $fileName =$File.Name;
-        $folderName =  $fileName -replace '\s','_';
-        $folderName = $folderName -replace  '\.', '-'
+        $folderName =  Generate-File-Folder -fileName $fileName
         Write-Host -f Green $folderName
-        $absoluteFolderPath = [System.IO.Path]::Combine([System.IO.Path]::Combine($currentDirectory, $folderName));
-        if(-Not (Test-Path $absoluteFolderPath)){
-            New-Item -Path $folderName -ItemType directory;
-            }
-        $latestVersionPath = [System.IO.Path]::Combine([System.IO.Path]::Combine($currentDirectory,$folderName), $fileName);
+        $absoluteFolderPath = [System.IO.Path]::Combine([System.IO.Path]::Combine($RootFolder, $folderName));
+        Create-Folder -folder $absoluteFolderPath
+        $latestVersionPath = [System.IO.Path]::Combine($absoluteFolderPath, $fileName);
         if(-Not (Test-Path $latestVersionPath))
         {
-            Get-PnPFile -Url $File.ServerRelativeUrl -Path $folderName -FileName $fileName -AsFile;
+            Get-PnPFile -Url $File.ServerRelativeUrl -Path $absoluteFolderPath -FileName $fileName -AsFile;
         }
         $versions = $File.Versions;
         $Ctx.load($versions);
@@ -66,15 +97,10 @@ Function Get-AllFilesFromFolder()
         foreach($version in $versions){
             $versionUrl = $version.Url;
             $versionFullURL = Combine-URI -uri1 $Ctx.Url -uri2 $versionUrl;
-            $currentDirectory =Get-Location;
-            $targetPath = [System.IO.Path]::Combine($currentDirectory,$folderName);
-            $targetPath = [System.IO.Path]::Combine($targetPath,$version.VersionLabel);
-            if(-Not (Test-Path $targetPath)){
-            New-Item -Path $targetPath -ItemType directory;
-            }
+            $targetPath = [System.IO.Path]::Combine($absoluteFolderPath ,$version.VersionLabel);
+            Create-Folder -folder $targetPath
             $targetPath = [System.IO.Path]::Combine($targetPath,$fileName);
-            if(-Not (Test-Path $targetPath)){
-            
+            if(-Not (Test-Path $targetPath)){            
                 $versionStream = $version.OpenBinaryStream();
                 $Ctx.Load($version);
                 $Ctx.ExecuteQuery();
@@ -86,13 +112,15 @@ Function Get-AllFilesFromFolder()
     }
           
     #Recursively Call the function to get files of all folders
-    $Ctx.load($Folder.Folders)
-    $Ctx.ExecuteQuery()
+    $Ctx.Load($Folder.Folders);
+    $Ctx.Load($Folder);
+    $Ctx.ExecuteQuery();
+    $folderRoot = [System.IO.Path]::Combine($RootFolder, $Folder.Name)
   
     #Exclude "Forms" system folder and iterate through each folder
     ForEach($SubFolder in $Folder.Folders | Where {$_.Name -ne "Forms"})
     {
-        Get-AllFilesFromFolder -Folder $SubFolder;
+        Get-AllFilesFromFolder -Folder $SubFolder -RootFolder $folderRoot;
     }
     
   }
@@ -101,7 +129,8 @@ Function Get-DocumentLibraryFiles()
 {
     param
     (
-        [Parameter(Mandatory=$true)] [Microsoft.SharePoint.Client.List] $Library
+        [Parameter(Mandatory=$true)] [Microsoft.SharePoint.Client.List] $Library,
+        [Parameter(Mandatory=$true)] [string] $RootFolder
     )
     Try {
       
@@ -111,7 +140,7 @@ Function Get-DocumentLibraryFiles()
         $ctx.ExecuteQuery();
   
         #Call the function to get Files of the Root Folder
-        Get-AllFilesFromFolder -Folder $Library.RootFolder
+        Get-AllFilesFromFolder -Folder $Library.RootFolder -rootFolder $RootFolder
      }
     Catch {
         write-host -f Red "Error:" $_.Exception.Message
@@ -126,9 +155,10 @@ $Ctx.Load($RootWeb);
 $Ctx.Load($RootWeb.Webs);
 $Ctx.Load($RootWeb.Lists);
 $Ctx.ExecuteQuery();
-Process-Web -web $RootWeb;
+$currentDirectory =Get-Location;
+$rootPath = Process-Web -web $RootWeb -rootFolder $currentDirectory;
 
 foreach($web in $RootWeb.Webs){
-    Process-Web -web $web    
+    Process-Web -web $web -rootFolder $rootPath
 }
 Disconnect-PnPOnline
